c9f679388384d3c68f0e54fe76a3f48fa43e4a94
brian.jo <brian.jo@kakaopaycorp.com>
2021-08-01 16:32:59 +0900

    api 호출 및 오류 처리 로직 구현

--- unsplash/Classes/Common/Network/NetworkClient.swift
+++ unsplash/Classes/Common/Network/NetworkClient.swift
@@ -8,37 +8,78 @@
 import Foundation
 
 enum QueryError: Error {
-    case auth
+    case badRequest
+    case unauthorized
+    case forbidden
+    case notFound
+    case unsplash
     case network(error: Error)
+    case parse
     case unknown
 }
 
 class NetworkClient {
-    typealias QueryResult = (Result<Any, QueryError>) -> Void
+    typealias QueryResult = Result<Any, QueryError>
+    typealias JSONDictionary = [String: Any]
     
     let session = URLSession(configuration: .default)
     
     var task: URLSessionDataTask?
     
-    func query(url: URL, completion: @escaping QueryResult) {
+    func query(url: URL, completion: @escaping (QueryResult) -> Void) {
         
         task?.cancel()
         
         task = session.dataTask(with: url) { [weak self] data, response, error in
+            guard let self = self else { return completion(.failure(.unknown)) }
+            
             defer {
-                self?.task = nil
+                self.task = nil
+            }
+            
+            if let data = data, let text = String(data: data, encoding: .utf8) {
+                print(text)
             }
             
             if let error = error {
                 return completion(.failure(.network(error: error)))
-            } else if let data = data,
-                      let response = response as? HTTPURLResponse,
-                      response.statusCode == 200 {
-                return completion(.success(data))
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                switch response.statusCode {
+                case 200:
+                    return completion(self.result(data))
+                case 400:
+                    return completion(.failure(.badRequest))
+                case 401:
+                    return completion(.failure(.unauthorized))
+                case 403:
+                    return completion(.failure(.forbidden))
+                case 404:
+                    return completion(.failure(.notFound))
+                default:
+                    return completion(.failure(.unsplash))
+                }
             } else {
                 return completion(.failure(.unknown))
             }
         }
         task?.resume()
+    }
+    
+    func result(_ data: Data) -> QueryResult {
+        var response: JSONDictionary?
+        
+        do {
+            response = try JSONSerialization.jsonObject(with: data, options: []) as? JSONDictionary
+        } catch let parseError as NSError {
+            print(parseError)
+            return .failure(.parse)
+        }
+        
+        guard let response = response else { return .failure(.unknown)}
+        
+        print(response)
+        
+        
+        return .success(data)
     }
 }
--- unsplash/Classes/Home/HomeViewController.swift
+++ unsplash/Classes/Home/HomeViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    // MARK: - Properties
+    private let networkClient = NetworkClient()
+    
+    // MARK: - Outlets
+    @IBOutlet weak var tableView: UITableView!
 
+    
+    // MARK: - View lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        title = "홈"
+        setup()
+    }
+    
+    // MARK: - Setups
+    private func setup() {
+        guard let url = URL(string: "https://api.unsplash.com/photos") else { return }
+        networkClient.query(url: url) { [weak self] result in
+            print(result)
+        }
     }
 
+}
 
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension HomeViewController: UITableViewDelegate {
+    
 }
