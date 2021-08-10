ffb15fe7649c296a3644f79fd731f88060e5d055
brian.jo <brian.jo@kakaopaycorp.com>
2021-08-10 13:41:45 +0900

    WebViewController 구현

---
+++ unsplash/Classes/Common/Web/WebViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WebViewController.swift
+//  unsplash
+//
+//  Created by mrtajo on 2021/08/10.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    
+}
