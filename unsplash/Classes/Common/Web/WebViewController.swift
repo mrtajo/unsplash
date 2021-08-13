0ce6950fb401c1eaa0d517360c67c7a3d7880928
brian.jo <brian.jo@kakaopaycorp.com>
2021-08-13 12:10:39 +0900

    WebViewController 에 샘플 페이지 로딩

--- unsplash/Classes/Common/Web/WebViewController.swift
+++ unsplash/Classes/Common/Web/WebViewController.swift
@@ -9,8 +9,40 @@ import UIKit
 import WebKit
 
 class WebViewController: UIViewController {
+    // MARK: - Properties
+    var urlString: String? = nil {
+        didSet { loadRequest() }
+    }
     
+    // MARK: - Outlets
     @IBOutlet weak var webView: WKWebView!
     
+    // MARK: - View lifecycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        urlString = "https://m.naver.com"
+    }
+    
+    // MARK: - Setups
+    private func setup() {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "웹 사이트 주소 입력"
+        navigationItem.titleView = searchBar
+    }
+    
+    // MARK: - Privates
+    private func loadRequest() {
+        guard let string = urlString, let url = URL(string: string) else { return }
+        
+        var request = URLRequest(url: url)
+        request.cachePolicy = .useProtocolCachePolicy
+        request.timeoutInterval = 30
+        
+        webView.load(request)
+    }
 }
